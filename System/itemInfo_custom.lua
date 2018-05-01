881a912604b2db6d3925e4cf1fa9df9e3eace1da
TheDerpySupport <Derpy-gaming@outlook.com>
2018-04-30 19:16:56 -0500

    Added HP/SP/Movement Elixirs

--- System/itemInfo_custom.lua
+++ System/itemInfo_custom.lua
@@ -1,22 +1,188 @@
 tbl = {
 	[32100] = {
-		unidentifiedDisplayName = "Lantern",
-		unidentifiedResourceName = "이상한랜턴",
+		unidentifiedDisplayName = "HP Elixir (Small)",
+		unidentifiedResourceName = "HP증가포션_소",
 		unidentifiedDescriptionName = { "" },
-		identifiedDisplayName = "Lantern",
-		identifiedResourceName = "이상한랜턴",
+		identifiedDisplayName = "HP Elixir (Small)",
+		identifiedResourceName = "HP증가포션_소",
 		identifiedDescriptionName = {
-			"A lantern to light your surroundings.",
-			"------------------------",
-			"[+^FF0000Sight Rune^000000]",
-			"Casts ^0000FFRuwach^000000 instead of ^FF0000Sight^000000.",
-			"------------------------",
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 25%^000000 HP",
+			"Delay: ^006600 10^000000 seconds",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32101] = {
+		unidentifiedDisplayName = "HP Elixir (Medium)",
+		unidentifiedResourceName = "HP증가포션_중",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "HP Elixir (Medium)",
+		identifiedResourceName = "HP증가포션_중",
+		identifiedDescriptionName = {
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 50%^000000 HP",
+			"Delay: ^006600 10^000000 seconds",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32102] = {
+		unidentifiedDisplayName = "HP Elixir (Large)",
+		unidentifiedResourceName = "HP증가포션_대",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "HP Elixir (Large)",
+		identifiedResourceName = "HP증가포션_대",
+		identifiedDescriptionName = {
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 75%^000000 HP",
+			"Delay: ^006600 10^000000 seconds",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32103] = {
+		unidentifiedDisplayName = "SP Elixir (Small)",
+		unidentifiedResourceName = "SP증가포션_소",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "SP Elixir (Small)",
+		identifiedResourceName = "SP증가포션_소",
+		identifiedDescriptionName = {
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 25%^000000 SP",
+			"Delay: ^006600 10^000000 seconds",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32104] = {
+		unidentifiedDisplayName = "SP Elixir (Medium)",
+		unidentifiedResourceName = "SP증가포션_중",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "SP Elixir (Medium)",
+		identifiedResourceName = "SP증가포션_중",
+		identifiedDescriptionName = {
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 50%^000000 SP",
+			"Delay: ^006600 10^000000 seconds",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32105] = {
+		unidentifiedDisplayName = "SP Elixir (Large)",
+		unidentifiedResourceName = "SP증가포션_대",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "SP Elixir (Large)",
+		identifiedResourceName = "SP증가포션_대",
+		identifiedDescriptionName = {
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 75%^000000 SP",
+			"Delay: ^006600 10^000000 seconds",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32106] = {
+		unidentifiedDisplayName = "Elixir (Small)",
+		unidentifiedResourceName = "HP증가포션_소",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Elixir (Small)",
+		identifiedResourceName = "HP증가포션_소",
+		identifiedDescriptionName = {
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 15%^000000 HP and SP",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32107] = {
+		unidentifiedDisplayName = "Elixir (Medium)",
+		unidentifiedResourceName = "HP증가포션_중",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Elixir (Medium)",
+		identifiedResourceName = "HP증가포션_중",
+		identifiedDescriptionName = {
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 25%^000000 HP and SP",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32108] = {
+		unidentifiedDisplayName = "Elixir (Large)",
+		unidentifiedResourceName = "HP증가포션_대",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Elixir (Large)",
+		identifiedResourceName = "HP증가포션_대",
+		identifiedDescriptionName = {
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 35%^000000 HP and SP",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32109] = {
+		unidentifiedDisplayName = "HP Elixir (Rental)",
+		unidentifiedResourceName = "HP증가포션_대",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "HP Elixir (Rental)",
+		identifiedResourceName = "HP증가포션_대",
+		identifiedDescriptionName = {
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 35%^000000 HP over 5 seconds",
+			"Delay: ^006600 10^000000 seconds",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32110] = {
+		unidentifiedDisplayName = "SP Elixir (Rental)",
+		unidentifiedResourceName = "SP증가포션_대",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "SP Elixir (Rental)",
+		identifiedResourceName = "SP증가포션_대",
+		identifiedDescriptionName = {
+			"^FFFFFF_^000000",
+			"Usable:^6666CC Restorative^000000",
+			"Heal:^006600 35%^000000 SP over 5 seconds",
+			"Delay: ^006600 10^000000 seconds",
+			"Weight:^006600 7^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[32111] = {
+		unidentifiedDisplayName = "Movement Elixir",
+		unidentifiedResourceName = "농축화이트포션Z",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Movement Elixir",
+		identifiedResourceName = "농축화이트포션Z",
+		identifiedDescriptionName = {
 			"^FFFFFF_^000000",
 			"Usable:^6666CC Supportive^000000",
-			"Effect:^006600 Detects hidden enemies^000000",
-			"Duration:^006600 10^000000 seconds",
-			"Delay: ^006600 1^000000 minute",
-			"Weight:^006600 20^000000"
+			"Effect:^006600 50%^000000 Movement Speed for 30 minutes",
+			"Weight:^006600 7^000000"
 		},
 		slotCount = 0,
 		ClassNum = 0
